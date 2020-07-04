69fb17e492aa568c23981f07c76c3e56ceb0411f
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-07-04 19:55:38 +0300

    removed process invalid requests from loop updated and process

--- scripts/data/loop_update_urls_and_process_requests.sh
+++ scripts/data/loop_update_urls_and_process_requests.sh
@@ -4,17 +4,12 @@ set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 cd "$DIR"
 
-PROCESS_REQUESTS_TIMES=5
 UPDATE_URLS_TIMES=5
 
 while true; do
-  for i in $(seq $PROCESS_REQUESTS_TIMES); do
-    for i in $(seq $UPDATE_URLS_TIMES); do
-      ./update_urls.sh
-    done
-
-    ./process_requests.sh
+  for i in $(seq $UPDATE_URLS_TIMES); do
+    ./update_urls.sh
   done
 
-  ./process_invalid_requests.sh
+  ./process_requests.sh
 done
